d9f96a62566569b064105a46655893720dd9e745
Alexander Megadrive <alexmihailov91@gmail.com>
2022-11-03 14:08:06 +0300

    feat: layout and title

--- scripts/htmlServices.js
+++ scripts/htmlServices.js
@@ -1,4 +1,4 @@
-function cutString(string = '', maxLength = 40) {
+function cutString(string = '', maxLength = 38) {
     if (string.length > maxLength) {
         return string.substring(0,maxLength) + '...'
     }
@@ -10,7 +10,7 @@ class HTMLService {
     paintProduct(product) {
         return `
         <li data-id="${product.id}">
-            <span><strong>${cutString(product.title, 40)}</strong></span>
+            <span><strong>${cutString(product.title, 38)}</strong></span>
             <span>${product.body}</span>
         </li>`
     }
