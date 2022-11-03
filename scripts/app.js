906174151bccd73365448b4a96542bb067fcb0d8
Alexander Megadrive <alexmihailov91@gmail.com>
2022-11-03 16:02:58 +0300

    refactor: app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,37 @@
 const htmlService = new HTMLService()
-// const cardServices = new cardService()
-
-
 const cardsContainer = document.getElementById('cards')
 const filterInput = document.getElementById('filter')
-
-
-async function startApplication() {
-    // renderCart()
-    const link = 'https://jsonplaceholder.typicode.com/posts/?_start=0&_limit=7'
-
-    const response = await fetch(link)
-    const data = await response.json()
-   
-    cardService = new cardService(data)
-    renderProducts(cardService.cards)
-
-   
-}
+const resultsTitle = document.getElementById('results-title')
+const params = (new URL(document.location)).searchParams;
+const query = params.get("query") || null;
 
 startApplication()
 
-function renderProducts(products) {
-    cardsContainer.innerHTML = htmlService.paintProducts(products)
+async function startApplication() {
+    const link = 'https://jsonplaceholder.typicode.com/posts/?_start=0&_limit=7'
+    const response = await fetch(link)
+    const data = await response.json()
+    cardService = new cardService(data)
+
+    if (query) {
+        const filteredProducts = cardService.filterBy(query)
+        resultsTitle.innerText = (filteredProducts.length > 0) ? `Результаты по запросу "${query}"` : `Ничего не найдено по запросу "${query}"`;
+        renderProducts(filteredProducts)
+    } else renderProducts(cardService.cards)
+
 }
 
+function renderProducts(cards) {
+    cardsContainer.innerHTML = htmlService.paintProducts(cards)
+}
+
+function applyFilter() {
+    location.href = filterInput.value ? `./index.html?query=${filterInput.value}` : `./index.html`
+}
+
+document.body.addEventListener('click', function (event) {
+    if (event.target.type == 'checkbox') {
+        const parent = event.target.closest('li')
+        parent.classList.toggle('card--checked')
+    }
+})
